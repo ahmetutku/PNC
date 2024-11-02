306a4fd3a8885c39634ff4bf992206122899e9a7
ahmetutku <ahamamcioglu2014@gmail.com>
2024-11-02 17:30:35 -0400

    Initial Commit

---
+++ pnc/pncApp.swift
@@ -0,0 +1,17 @@
+//
+//  pncApp.swift
+//  pnc
+//
+//  Created by Ahmet Hamamcioglu on 2.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct pncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
