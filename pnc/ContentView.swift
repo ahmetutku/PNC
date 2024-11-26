352d2aabd6064b80b6daf39ccc7ebe83e54a4e1e
ahmetutku <ahamamcioglu2014@gmail.com>
2024-11-25 22:25:45 -0500

    added more placeholder quotes

--- pnc/ContentView.swift
+++ pnc/ContentView.swift
@@ -156,6 +156,10 @@ struct Quote: Identifiable, Equatable {
     static let sampleQuotes: [Quote] = [
         Quote(text: "The only limit to our realization of tomorrow is our doubts of today.", author: "Franklin D. Roosevelt"),
         Quote(text: "Success is not final, failure is not fatal: It is the courage to continue that counts.", author: "Winston Churchill"),
+        Quote(text: "Do not wait to strike till the iron is hot; but make it hot by striking.", author: "William Butler Yeats"),
+        Quote(text: "Do not wait to strike till the iron is hot; but make it hot by striking.", author: "William Butler Yeats"),
+        Quote(text: "Do not wait to strike till the iron is hot; but make it hot by striking.", author: "William Butler Yeats"),
+        Quote(text: "Do not wait to strike till the iron is hot; but make it hot by striking.", author: "William Butler Yeats"),
         Quote(text: "Do not wait to strike till the iron is hot; but make it hot by striking.", author: "William Butler Yeats")
         // Add more quotes if needed
     ]
