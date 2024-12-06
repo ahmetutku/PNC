4c679513c517e93ed6c2ae27beb5201a7abeffa1
ahmetutku <ahamamcioglu2014@gmail.com>
2024-12-05 23:03:38 -0500

    Added quotes and removed exmaple html content from quoteParser

--- pnc/bookData/quoteParser.py
+++ pnc/bookData/quoteParser.py
@@ -2,28 +2,7 @@ from bs4 import BeautifulSoup
 
 # Input HTML content
 html_content = """
-<div class="quoteText">
-      &ldquo;Time isn’t precious at all, because it is an illusion. What you perceive as precious is not time but the one point that is out of time: the Now. That is precious indeed. The more you are focused on time—past and future—the more you miss the Now, the most precious thing there is.&rdquo;
-    <br />
-  &#8213;
-  <span class="authorOrTitle">
-    Eckhart Tolle,
-  </span>
-    <span id=quote_book_link_6708>
-      <a class="authorOrTitle" href="/work/quotes/840520">The Power of Now: A Guide to Spiritual Enlightenment</a>
-    </span>
-</div>
-<div class="quoteText">
-      &ldquo;I have lived with several Zen masters -- all of them cats.&rdquo;
-    <br />
-  &#8213;
-  <span class="authorOrTitle">
-    Eckhart Tolle,
-  </span>
-    <span id=quote_book_link_6708>
-      <a class="authorOrTitle" href="/work/quotes/840520">The Power of Now: A Guide to Spiritual Enlightenment</a>
-    </span>
-</div>
+
 """
 
 # Parse the HTML content using BeautifulSoup
