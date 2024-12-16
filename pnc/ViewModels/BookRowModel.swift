a42b13d89e3e0e6ab4411dd1986fc3c33ff06782
ahmetutku <ahamamcioglu2014@gmail.com>
2024-12-16 18:24:24 -0500

    added init to bookrowmodel

--- pnc/ViewModels/BookRowModel.swift
+++ pnc/ViewModels/BookRowModel.swift
@@ -4,11 +4,14 @@
 //
 //  Created by Ahmet Hamamcioglu on 9.12.2024.
 //
-
 import SwiftUI
 
 class BookRowModel: ObservableObject {
-    @Published var favoriteBooks: [Book] = [] // Saved favorite quotes
+    @Published var favoriteBooks: [Book]
+
+    init(favoriteBooks: [Book] = []) {
+        self.favoriteBooks = favoriteBooks
+    }
 
     func addToMyBooks(_ book: Book) {
         if favoriteBooks.contains(where: { $0.id == book.id }) {
