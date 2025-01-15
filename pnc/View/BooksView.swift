02d329c8dfa12fc30eb614be3ee7bc59f0ef2fc1
ahmetutku <ahamamcioglu2014@gmail.com>
2025-01-14 19:26:23 -0500

    Improved placement of the book view texts and added book view preview

--- pnc/View/BooksView.swift
+++ pnc/View/BooksView.swift
@@ -17,28 +17,44 @@ struct BooksView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .padding(.top)
-            if(bookRowModel.favoriteBooks.isEmpty){
-                Spacer()
-                Text("Favorited Books is Empty")
-                Spacer()
-            }
 
-            ScrollView {
-                ForEach(bookRowModel.favoriteBooks) { book in
-                    NavigationLink(destination: BookDetailView(book: book, model: bookRowModel)) {
-                        BookRowView(book: book, model: bookRowModel)
+            if bookRowModel.favoriteBooks.isEmpty {
+                Spacer() // Push the empty state message to the center
+                Text("Favorited Books is Empty")
+                    .multilineTextAlignment(.center)
+                Spacer(minLength: 350)// Push it back up
+            } else {
+                ScrollView {
+                    ForEach(bookRowModel.favoriteBooks) { book in
+                        NavigationLink(destination: BookDetailView(book: book, model: bookRowModel)) {
+                            BookRowView(book: book, model: bookRowModel)
+                        }
                     }
-                    
-                    
-                }.padding()
-            }.listStyle(PlainListStyle())
-            Spacer()
-        }.onAppear(){
+                    .padding()
+                }
+                .listStyle(PlainListStyle())
+            }
+        }
+        .onAppear {
             books = BookLoader.loadBooks()
             print(books.count)
-
         }
         .padding()
         .background(Color("beigeColor").ignoresSafeArea())
+
+    }
+}
+
+struct BookView_Previews: PreviewProvider {
+    static var previews: some View {
+        // Create mock books
+        
+        // Create a mock BookRowModel with favorite books
+        let mockBookRowModel = BookRowModel()
+        mockBookRowModel.favoriteBooks = []
+        
+        return NavigationView {
+            BooksView(bookRowModel: mockBookRowModel)
+        }
     }
 }
