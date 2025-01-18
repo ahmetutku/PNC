f193177d959dd16c8f9ca9cefbff99b838060646
ahmetutku <ahamamcioglu2014@gmail.com>
2025-01-17 20:21:01 -0500

    Added conditional to not display introductory messages after books have been added ( seems to be some display issue)

--- pnc/View/ContentView.swift
+++ pnc/View/ContentView.swift
@@ -12,36 +12,57 @@ struct ContentView: View {
                 Color("beigeColor").ignoresSafeArea()
 
                 VStack(spacing: 1) {
+                    if(quotesViewModel.quotes.count == 0){
+                        Text("Welcome To Project PNC")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                            .padding(.top)
 
-                    Text("Welcome To Project PNC")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                        .foregroundColor(.accentColor)
-                        .padding(.top)
+                        Spacer(minLength: 20)
 
-                    Spacer(minLength: 20)
+                        Text("PNC is made as an alternative to scrolling on social media. We believe that your desire to scroll should not keep you from education.")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color(.subtitle))
+                            .padding(.top,-80)
+                            .padding(.horizontal, 35.0)
+                            .foregroundColor(.gray)
 
-                    Text("PNC is made as an alternative to scrolling on social media. We believe that your desire to scroll should not keep you from education.")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color(.subtitle))
-                        .padding(.top,-80)
-                        .padding(.horizontal, 35.0)
-                        .foregroundColor(.gray)
+                        Spacer(minLength: 20)
 
-                    Spacer(minLength: 20)
+                        Text("You can get started with adding books using the navigation bar on the top left of your screen.")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .multilineTextAlignment(.center)
+                            .foregroundColor(Color(.subtitle))
+                            .padding(.top,-80)
+                            .padding(.horizontal, 35)
+                            .foregroundColor(.gray)
 
-                    Text("You can get started with adding books using the navigation bar on the top left of your screen.")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .multilineTextAlignment(.center)
-                        .foregroundColor(Color(.subtitle))
-                        .padding(.top,-80)
-                        .padding(.horizontal, 35)
-                        .foregroundColor(.gray)
-
-                    Spacer()
+                        Spacer()
+                    }
+                    else{
+                        Spacer()
+                        Text("PNC")
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                            .padding(.top)
+                        Spacer()
+                        ForEach(quotesViewModel.quotes) { quote in
+                            QuoteRowView(
+                                quote: quote,
+                                isFavorite: quotesViewModel.favoriteQuotes.contains(quote)
+                            ) {
+                                quotesViewModel.toggleFavorite(quote)
+                            }
+                            .padding()
+                        }
+                        Spacer()
+                        }
+                    
                 }
                 .frame(maxWidth: .infinity)
                 .padding()
